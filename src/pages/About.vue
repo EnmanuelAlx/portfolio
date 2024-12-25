b766a29c003d3d370d82c97a9a742b4a716a6b0b
Enmanuel Madrid <enmanuelalx@gmail.com>
2024-12-25 19:48:17 -0400

    init project

---
+++ src/pages/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <TemplateSection>
+    <div class="content">
+      <h1>About</h1>
+      <p>
+        Ingeniero en Informática, graduado de la Universidad Católica Andrés Bello, con una pasión
+        por enfrentar retos y aprender constantemente. Cuento con experiencia en arquitecturas de
+        software y patrones de diseño, desarrollando soluciones de alta calidad en colaboración con
+        equipos multidisciplinarios para crear sistemas óptimos y fáciles de mantener. Actualmente
+        me desempeño como Tech Lead, con un enfoque principal en ingeniería backend, donde he
+        liderado proyectos complejos y estratégicos. Además, soy un entusiasta de la seguridad
+        informática, especialmente en el ámbito del pentesting, combinando mi interés en la
+        ciberseguridad con mi trayectoria técnica para ofrecer soluciones seguras y eficientes.
+      </p>
+    </div>
+  </TemplateSection>
+</template>
+
+<script>
+import TemplateSection from '@/components/TemplateSection.vue'
+
+export default {
+  name: 'AboutMe',
+  components: {
+    TemplateSection,
+  },
+}
+</script>
+
+<style scoped>
+.content {
+  background-color: yellow;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <TemplateSection class="content">
+    <div class="text-container">
+      <h1>
+        Tech Lead apasionado por los retos y el humor bien
+        codificado.
+      </h1>
+    </div>
+  </TemplateSection>
+</template>
+
+<script>
+import TemplateSection from '@/components/TemplateSection.vue'
+
+export default {
+  name: 'VueHeader',
+  components: {
+    TemplateSection,
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 4rem;
+}
+.content {
+  background-color: black;
+  height: 100vh;
+  align-content: center;
+  display: grid;
+  place-content: center;
+  text-align: center;
+  color: white;
+  background-image: url('@/assets/images/background.jpg');
+}
+.text-container {
+  max-width: 60vw;
+  margin: 0 auto;
+  font-weight-bold: 700;
+}
+</style>
