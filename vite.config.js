cdc0920a3f9de7ff19067748cc650d3149d2b700
EnmanuelAlx <EnmanuelAlx@gmail.com>
2024-12-25 22:59:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'http://enmanuelalx.com',
+  base: 'https://enmanuelalx.com',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
