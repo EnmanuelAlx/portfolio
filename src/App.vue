848927833a24838d1a73675e9a9d4deba69d99c1
Enmanuel Madrid <enmanuelalx@gmail.com>
2024-12-25 21:30:10 -0400

    Revert "Deploying to master from @ EnmanuelAlx/portfolio@c4d50de3d26aee03a0715894546e78d8e73beed1 🚀"
    This reverts commit 3d3953e122fd8ba4c2d3bca7dd9e91859ad5a456.

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Home />
+  <AboutMe />
+  <PersonalExperience />
+</template>
+
+<script setup>
+import AboutMe from '@/pages/About.vue'
+import Home from '@/pages/Home.vue'
+import PersonalExperience from '@/pages/PersonalExperience.vue'
+</script>
+<style scoped></style>
